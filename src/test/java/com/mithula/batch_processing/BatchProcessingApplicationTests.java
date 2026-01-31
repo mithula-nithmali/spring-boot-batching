51bd25070bb82f4617c38db387094f1b65bf4a33
mithula <mithula@dmsswt.com>
2026-01-31 14:59:52 +0530

    add dependencied

---
+++ src/test/java/com/mithula/batch_processing/BatchProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mithula.batch_processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
